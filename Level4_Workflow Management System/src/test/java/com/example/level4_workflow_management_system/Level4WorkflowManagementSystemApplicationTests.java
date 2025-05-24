ba89e377b05d78fb201f6093fcfa48356e12cf45
Carrotww <rjgjfl90@naver.com>
2025-05-24 13:06:00 +0900

    Add_Project : Level4 workflow

---
+++ Level4_Workflow Management System/src/test/java/com/example/level4_workflow_management_system/Level4WorkflowManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.level4_workflow_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Level4WorkflowManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
