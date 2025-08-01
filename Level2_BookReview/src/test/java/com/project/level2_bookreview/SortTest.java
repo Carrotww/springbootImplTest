4623948889d2a4fbfab803eb3df39cfa9bf662cc
Carrotww <rjgjfl90@naver.com>
2025-08-01 16:55:20 +0900

    add sort test

---
+++ Level2_BookReview/src/test/java/com/project/level2_bookreview/SortTest.java
@@ -0,0 +1,29 @@
+package com.project.level2_bookreview;
+
+import org.junit.jupiter.api.Test;
+
+public class SortTest {
+
+    static class Student {
+
+        int age;
+        String name;
+        char level;
+
+        Student(int i, String s, char c) {
+            this.age = i;
+            this.name = s;
+            this.level = c;
+        }
+    }
+
+    @Test
+    void comparableTest() {
+
+    }
+
+    @Test
+    void comparatorTest() {
+
+    }
+}
