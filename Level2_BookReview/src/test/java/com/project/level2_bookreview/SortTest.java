a86e856d42cd086202e2c554be2179dbd00c716c
Carrotww <rjgjfl90@naver.com>
2025-08-07 17:58:12 +0900

    sorttest

--- Level2_BookReview/src/test/java/com/project/level2_bookreview/SortTest.java
+++ Level2_BookReview/src/test/java/com/project/level2_bookreview/SortTest.java
@@ -1,6 +1,10 @@
 package com.project.level2_bookreview;
 
 import org.junit.jupiter.api.Test;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.io.*;
 
 public class SortTest {
 
@@ -17,9 +21,26 @@ public class SortTest {
         }
     }
 
+    static int[] intAry = {5, 1, 4, 3, 2};
+
+    @Test
+    void test() {
+        Stream<Integer> stream = Arrays.stream(intAry).boxed();
+        System.out.println(stream);
+        Integer[] ary = stream.toArray(Integer[]::new);
+        System.out.println(Arrays.toString(ary));
+
+        Integer[] ary2 = Arrays.stream(intAry).boxed().toArray(Integer[]::new);
+        Arrays.sort(ary2, Collections.reverseOrder());
+
+        Integer[] ary3 = Arrays.stream(intAry).boxed().toArray(size -> new Integer[size]);
+    }
+
     @Test
     void comparableTest() {
+        Function<Integer, String> impleFunction = x -> "value" + x;
 
+        System.out.println(impleFunction.apply(10));
     }
 
     @Test
