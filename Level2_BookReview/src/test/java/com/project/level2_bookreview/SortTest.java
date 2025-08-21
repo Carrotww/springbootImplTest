04664e1472421925ec37c385810d335c951071a3
Carrotww <rjgjfl90@naver.com>
2025-08-21 17:57:11 +0900

    temp

--- Level2_BookReview/src/test/java/com/project/level2_bookreview/SortTest.java
+++ Level2_BookReview/src/test/java/com/project/level2_bookreview/SortTest.java
@@ -3,7 +3,7 @@ package com.project.level2_bookreview;
 import org.junit.jupiter.api.Test;
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.stream.*;
 import java.io.*;
 
 public class SortTest {
@@ -22,6 +22,7 @@ public class SortTest {
     }
 
     static int[] intAry = {5, 1, 4, 3, 2};
+    static Integer[] IntegerAry = {6, 1, 4, 56, 2, 1};
 
     @Test
     void test() {
@@ -34,6 +35,13 @@ public class SortTest {
         Arrays.sort(ary2, Collections.reverseOrder());
 
         Integer[] ary3 = Arrays.stream(intAry).boxed().toArray(size -> new Integer[size]);
+
+        System.out.println("-----------------");
+        Arrays.sort(intAry);
+        Arrays.sort(IntegerAry);
+        System.out.println(IntegerAry);
+        Arrays.sort(IntegerAry, Collections.reverseOrder());
+        System.out.println(IntegerAry);
     }
 
     @Test
@@ -46,5 +54,32 @@ public class SortTest {
     @Test
     void comparatorTest() {
 
+    }
+
+    @Test
+    void boxedTest() {
+        int N = 10_000_000;
+
+        long start1 = System.currentTimeMillis();
+        long sum1 = Stream.iterate(0, i -> i + 1)
+                          .limit(N)
+                          .mapToInt(Integer::intValue)
+                          .sum();
+        long end1 = System.currentTimeMillis();
+        System.out.println("Stream<Integer> 합계: " + sum1 + ", 실행 시간: " + (end1 - start1) + "ms");
+
+        long start2 = System.currentTimeMillis();
+        long sum2 = IntStream.range(0, N)
+                             .sum();
+        long end2 = System.currentTimeMillis();
+        System.out.println("IntStream 합계: " + sum2 + ", 실행 시간: " + (end2 - start2) + "ms");
+
+        long start3 = System.currentTimeMillis();
+        long sum3 = IntStream.range(0, N)
+                             .boxed()
+                             .mapToInt(Integer::intValue)
+                             .sum();
+        long end3 = System.currentTimeMillis();
+        System.out.println("IntStream.boxed() 합계: " + sum3 + ", 실행 시간: " + (end3 - start3) + "ms");
     }
 }
